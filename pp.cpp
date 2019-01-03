282ebfd044956ee0e48f29d9430091141d2033d2
kilodelta8 <kilo.kilo.delta8@gmail.com>
2019-01-03 15:17:45 -0500

    why?

--- pp.cpp
+++ pp.cpp
@@ -7,176 +7,72 @@
 #include <chrono>
 #include <ctime>
 
-//Classes using wiring pi
-class Gpio{
-    private:
-        std::string pinName;
-        int pinNumber;
-        bool pinState;
-    public:
-        void setPinInOut();
-        void setPinName(std::string name);
-        void setPinNumber(int *pNums, int number);
-        void setPinState(int x);
-        std::string getPinName();
-        int getPinNumber();
-        bool getPinState();
-        void displayInfo();
-};
-//sets the pin as input or output
-void Gpio::setPinInOut(){
-    char ans;
-    int pNum = getPinNumber();
-    std::string pn = getPinName();
-    std::cout << "Is this pin for input (i) or output (o): ";
-    std::cin >> ans;
-    if (ans == 'i'){
-        if (pNum == NULL){
-            std::cout << "You must set the pin name and number first.\n";
-        } else {
-            if (pn == ""){
-                std::cout << "You must set the pin name and number first.\n";
-            } else {
-                pinMode(pNum, INPUT);
-            }
-        }
-    } else if (ans == 'o'){
-        if (pNum == NULL){
-            std::cout << "You must set the pin name and number first.\n";
-        } else {
-            if (pn == ""){
-                std::cout << "You must set the pin name and number first.\n";
-            } else {
-                pinMode(pNum, OUTPUT);
-            }
-        }
-    } else {
-        std::cout << "An error has occurred while setting the pin to in or out.\n";
-        //how can i activate a logging function/class??
-    }
-}
-//sets the pin name
-void Gpio::setPinName(std::string name){
-    pinName = name;
-}
-//sets the pin number
-void Gpio::setPinNumber(int *pNums, int number){
-    int count = 0;
-    for (int i = 0; i < 40; i++){
-        if (pNums[i] == number){
-            pNums[i] == 0;
-            pinNumber = number;
-        } else if (pNums[i] == 0){
-            std::cout << "That number is already in use.\n";
-            break;
-        } else {
-            std::cout << "Something else occurred while setting pin number.\n";
-            //logger?
-        }
-    }
-}
-//sets the pin state
-void Gpio::setPinState(int x){
-    int num = getPinNumber();
-    pinState = digitalRead(num);
-    if (x == 0){
-        digitalWrite(num, LOW);
-    } else if (x == 1){
-        digitalWrite(num, HIGH);
-    } else {
-        std::cout << "Some error occurred while setting the pin state!\n";
-    }
-}
-//returns the pin name
-std::string Gpio::getPinName(){
-    return pinName;
-}
-//returns the pin number
-int Gpio::getPinNumber(){
-    return pinNumber;
-}
-//returns the pin state
-bool Gpio::getPinState(){
-    pinState = digitalRead(getPinNumber());
-    return pinState;
-}
-//displays the pin info
-void Gpio::displayInfo(){
-    std::cout << "Pin Name: " << pinName << "\n";
-    std::cout << "Pin Numb: " << pinNumber << "\n";
-    std::cout << "Pin Stat: " << pinState << "\n";
-}
-
-//Class to record data-------------not even started yet---------------------------------LOGGER
-class Logger{
-    private:
-    public:
-    //is this necassary?????
-    //maybe a function would suffice??
-};
 
 //function prototype----------------------------------------------------------gteTime() proto
-int getTime();
+std::string time();
+void log(std::ofstream &outFile, std::string data);
 
-//##################################################################################MAIN
-int main(){
-    //array to keep track of used/unused pin numbers
-    int pNums[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-    17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,
-    35,36,37,38,39,40};
 
-    //variables
-    std::time_t t = std::time(0);//<-------logs what time the program started
-    std::tm* now = std::localtime(&t);
 
+int main()
+{
+    
     //setup a file for logging
-    std::ofstream outFile;
-    outFile.open("/home/kd8/cpp/piggpi/datalog.txt", std::ios::app);
-
+    std::ofstream outFile; //<-----all ops for outFile are carried out in log()
+    
     //Setup wiringPi
-    int setupValue = wiringPiSetup();
+    wiringPiSetup();
 
     //check to see if wiring pi setup correctly
-    if (!setupValue){
-        std::cout << "WiringPi failed to setup properly. . . . \n";//let you know
-        std::cout << "Logging and Exiting the program. . . . .\n";
-        if (!outFile.is_open()){ //<---------------------------------make sure logging file opened
-            std::cout << "Failed to log failures. . . . .. \n";
-        } else if (outFile.is_open()){
-            outFile << getTime();//<--------------------------------note the time and error to log
-            outFile << "ERROR: WiringPi failed to load.";
-            outFile.close();
-        } else {
-            std::cout << "Some other error occurred!!!\n";
-        }
+    if (!wiringPiSetup()){
+        std::cout << "WiringPi failed to setup properly, Exiting the program!\n";//let you know
+        log(outFile, "Failed to setup wiringPi()");
         return 1;
+    }
+    else if (wiringPiSetup())
+    {
+        log(outFile, "wiringPiSetup() initialzed successfully");
+    }
+    else
+    {
+        std::cout << "Some other failure has occurred!\n";
+        log(outFile, "Else executed, unknown reason!");
     }
 
     //Setup pins
-    Gpio waterValve;
-    Gpio waterSensor1;
-    Gpio waterSensor2;
-    Gpio alertLed1;
-    alertLed1.setPinNumber(pNums, 0);
-    alertLed1.setPinName("WaterSensor1led");
-    Gpio alertLed2;
-    alertLed2.setPinNumber(pNums, 4);
-    alertLed2.setPinName("WaterSensor2led");
-    Gpio alertLed3;
-    alertLed3.setPinNumber(pNums, 7);
-    alertLed3.setPinName("WaterSensor3led");
-
-
-    //clean up
-    outFile.close();
+    //run the program until quit
 
     return 0;
 }
 
-//----------->>FUNCTIONS<<----------------------------------------------------------------------FUNCTION(S)
-int getTime(){
-    std::time_t t = std::time(0);//<-------logs what time the program started
-    std::tm* now = std::localtime(&t);
-    int date = ((now->tm_mon + 1) + (now->tm_mday) + (now->tm_year + 1900));
-    return date;
+//----------->>FUNCTIONS<<--------------------------------------------------------------
+
+//This functions gets the current time and returns it as a string
+std::string time()
+{
+    // declaring argument of time() 
+    time_t my_time = time(NULL); 
+    // ctime() used to give the present time 
+    std::string time = ctime(&my_time); 
+    return time;
+}
+
+//log() takes the OFSTREAM object and a string as an argument and appends both to
+//a text file specified in the function.  It also calls the time() function and adds
+//the date and time the log was made to the file.
+void log(std::ofstream &outFile, std::string data)
+{
+    //dont forget to create the file object in main
+    outFile.open("/home/kd8/cpp/piggypi/testData.txt", std::ios::app);
+    //check to make sure the file has opened correctly
+    if (!outFile.is_open())
+    {
+        std::cout << "Log() Failed to open file...\n";//<----let you know via console
+    }
+    else if (outFile.is_open())
+    {
+        outFile << time() << data << std::endl; //<------log the data to the file
+    }
+    //close the file
+    outFile.close();
 }
